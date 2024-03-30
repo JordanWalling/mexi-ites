2eb0278ea92b290679e21907e3ffd05cd878c009
JordanWalling <jordanwalling1991@gmail.com>
2024-03-30 11:00:17 +1100

    create Footer component and apply to Home Layout

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import "../Footer/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer__wrapper">
+      <p>Copyright 2024 - Jordan Walling &copy;</p>
+    </div>
+  );
+}
+export default Footer;
--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -1,10 +1,12 @@
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 function HomeLayout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
