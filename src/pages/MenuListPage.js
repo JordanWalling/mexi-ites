5fa66d19931277e3c070e5c304807d6762463460
JordanWalling <jordanwalling1991@gmail.com>
2024-03-31 09:36:33 +1100

    Add loader spinner for menuListPage

--- src/pages/MenuListPage.js
+++ src/pages/MenuListPage.js
@@ -4,12 +4,15 @@ import MenuTitle from "../components/MenuTitle/MenuTitle";
 import MenuListItemCard from "../components/MenuListItemCard/MenuListItemCard";
 import FilterButtons from "../components/FilterButtons/FilterButtons";
 import filters from "../data";
+import { Audio } from "react-loader-spinner";
 function MenuListPage() {
   const [items, setItems] = useState([]);
   const [category, setCategory] = useState("mains");
+  const [isLoading, setIsLoading] = useState(false);
 
   //
   async function fetchItems() {
+    setIsLoading(true);
     const resp = await fetch("https://backend-api-rh76.onrender.com/items", {
       method: "GET",
       headers: {
@@ -19,6 +22,7 @@ function MenuListPage() {
     });
     const data = await resp.json();
     setItems(data);
+    setIsLoading(false);
   }
   useEffect(() => {
     fetchItems();
@@ -37,6 +41,20 @@ function MenuListPage() {
       <MenuTitle />
       <FilterButtons setCategoryChange={setCategoryChange} filters={filters} />
       <section className="menulist__section">
+        {isLoading ? (
+          <div className="menulist__loader">
+            <Audio
+              height="80"
+              width="80"
+              radius="9"
+              //   color="green"
+              color="#ffd204"
+              ariaLabel="loading"
+              wrapperStyle
+              wrapperClass
+            />
+          </div>
+        ) : null}
         <ul>
           {filteredItems.map((item) => {
             return <MenuListItemCard item={item} key={item.id} />;
